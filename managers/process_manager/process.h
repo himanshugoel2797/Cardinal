6ebde72209b37e3ca1d4c9e98fe0ed5bcfea56f8
Himanshu Goel <himanshu.goel2797@gmail.com>
2016-01-18 12:13:52 -0400

    Defined HAL for virtual memory management and interface for Process management

--- managers/process_manager/process.h
+++ managers/process_manager/process.h
@@ -14,17 +14,78 @@ typedef enum{
 }ProcessStatus;
 
 typedef enum{
-
+  ProcessPriority_Normal,
+  ProcessPriority_High
 }ProcessPriority;
 
-typedef struct{
-  UID ID;
-  char Name[MAX_PROCESS_NAME_LEN];
-  ProcessStatus Status;
-  ProcessPriority Priority;
-  UID PageTable;
-  MemoryAllocationsMap *AllocationMap;
+typedef enum{
+  ProcessPermissions_None = 0,
+  ProcessPermissions_NormalFileIO = (1 << 0),
+  ProcessPermissions_RootFileIO = (1 << 1),
+  ProcessPermissions_Kernel = (1 << 2),
+  ProcessPermissions_User = (1 << 3),
+  ProcessPermissions_SuperUser = (1 << 4),
+  ProcessPermissions_Network = (1 << 5)
+}ProcessPermissions;
+
+typedef struct ProcessInformation{
+  UID 				ID;
+  char 				Name[MAX_PROCESS_NAME_LEN];
+  ProcessStatus 		Status;
+  ProcessPriority 		Priority;
+  ProcessPermissions 		Permissions;
+  UID 				PageTable;
+  MemoryAllocationsMap 		*AllocationMap;
+  struct ProcessInformation 	**children;
 }ProcessInformation;
 
+typedef enum{
+  ProcessErrors_None = 0,
+  ProcessErrors_Unknown = (1 << 0)
+}ProcessErrors;
+
+typedef enum{
+  ProcessSignals_SIGABRT = (1 << 0),
+  ProcessSignals_SIGALRM = (1 << 1),
+  ProcessSignals_SIGVTALRM = (1 << 2),
+  ProcessSignals_SIGPROF = (1 << 3),
+  ProcessSignals_SIGBUS = (1 << 4),
+  ProcessSignals_SIGCHLD = (1 << 5),
+  ProcessSignals_SIGCONT = (1 << 6),
+  ProcessSignals_SIGFPE = (1 << 7),
+  ProcessSignals_SIGHUP = (1 << 8),
+  ProcessSignals_SIGILL = (1 << 9),
+  ProcessSignals_SIGINT = (1 << 10),
+  ProcessSignals_SIGKILL = (1 << 11),
+  ProcessSignals_SIGPIPE = (1 << 12),
+  ProcessSignals_SIGPOLL = (1 << 13),
+  ProcessSignals_SIGQUIT = (1 << 14),
+  ProcessSignals_SIGSEGV = (1 << 15),
+  ProcessSignals_SIGSTOP = (1 << 16),
+  ProcessSignals_SIGSYS = (1 << 17),
+  ProcessSignals_SIGTERM = (1 << 18),
+  ProcessSignals_SIGTRAP = (1 << 19),
+  ProcessSignals_SIGXFSZ = (1 << 20),
+  ProcessSignals_SIGWINCH = (1 << 21)
+}ProcessSignals;
+
+ProcessErrors
+ForkProcess(ProcessInformation *src,
+	    ProcessInformation *dst);
+
+ProcessErrors
+GetProcessInformation(UID 			pid,
+		      ProcessInformation	*procInfo);
+
+ProcessErrors
+KillProcess(UID pid);
+
+ProcessErrors
+SleepProcess(UID pid);
+
+ProcessErrors
+RegisterSignalHandler(UID 		pid,
+		      ProcessSignals 	signals,
+		      void 		(*sigHandler)(ProcessSignals));
 
 #endif
--- target/hal/memory.h
+++ target/hal/memory.h
@@ -14,23 +14,38 @@ typedef enum{
   MemoryAllocationType_Heap,
   MemoryAllocationType_Stack,
   MemoryAllocationType_MMap,
+  MemoryAllocationType_Fork,
   MemoryAllocationType_Paged,
   MemoryAllocationType_Shared
 }MemoryAllocationType;
 
 typedef enum{
-  MemoryAllocationFlags_NoExec,
-  MemoryAllocationFlags_Exec,
-  MemoryAllocationFlags_Write
+  MemoryAllocationFlags_NoExec = (1 << 0),
+  MemoryAllocationFlags_Exec = (1 << 1),
+  MemoryAllocationFlags_Write = (1 << 2),
+  MemoryAllocationFlags_Kernel = 0,
+  MemoryAllocationFlags_User = (1 << 3)
 }MemoryAllocationFlags;
 
+typedef enum{
+  MemoryAllocationErrors_None = 0,
+  MemoryAllocationErrors_AlreadyMapped = (1 << 0),
+  MemoryAllocationErrors_NotMapped = (1 << 1),
+  MemoryAllocationErrors_PartiallyMapped = (1 << 2),
+  MemoryAllocationErrors_InvalidFlags = (1 << 3),
+  MemoryAllocationErrors_InvalidVirtualAddress = (1 << 4),
+  MemoryAllocationErrors_InvalidPhysicalAddress = (1 << 5),
+  MemoryAllocationErrors_Unknown = (1 << 6)
+}MemoryAllocationErrors;
+
 typedef struct MemoryAllocationsMap{
-  CachingMode CacheMode;
-  uint64_t VirtualAddress;
-  uint64_t Length;
+  CachingMode 		CacheMode;
+  uint64_t	 	VirtualAddress;
+  uint64_t 		Length;
   MemoryAllocationFlags Flags;
-  MemoryAllocationType AllocationType;
-  uint64_t AdditionalData;
+  MemoryAllocationType 	AllocationType;
+  uint64_t 		AdditionalData;
+  uint64_t 		ReferenceCount;
 
   struct MemoryAllocationsMap *next;
 }MemoryAllocationsMap;
@@ -41,5 +56,35 @@ GetVirtualAddress(CachingMode 	c,
 
 void*
 GetPhysicalAddress(void *virtualAddress);
+
+MemoryAllocationErrors
+CreateVirtualMemoryInstance(UID *inst);
+
+UID
+SetActiveVirtualMemoryInstance(UID inst);
+
+UID
+GetActiveVirtualMemoryInstance(void);
+
+MemoryAllocationErrors
+MapPage(UID 			pageTable,
+	MemoryAllocationsMap	*allocationMap,
+	uint64_t 		physicalAddress,
+	uint64_t 		virtualAddress,
+	size_t			size,
+	CachingMode 		cacheMode,
+	MemoryAllocationType 	allocType,
+	MemoryAllocationFlags 	flags);
+
+MemoryAllocationErrors
+UnmapPage(UID 			pageTable,
+	  MemoryAllocationsMap *allocationMap,
+	  uint64_t 		virtualAddress);
+
+MemoryAllocationErrors
+FindFreeVirtualAddress(UID 			pageTable,
+		       size_t 			size,
+		       MemoryAllocationType 	allocType,
+		       MemoryAllocationFlags 	flags);
 
 #endif
