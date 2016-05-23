14dd0261951d96d97d393e2b74a20b87660430a0
Himanshu Goel <himanshu.goel2797@gmail.com>
2016-05-22 21:39:46 -0400

    More work on notification passing mechanism

--- managers/notification_manager/notifications.c
+++ managers/notification_manager/notifications.c
@@ -1,1 +1,40 @@
 #include "notifications.h"
+
+typedef struct NotificationProviderData{
+	NotificationType type;
+	uint64_t event;
+	NotificationProvider subscriptionHandler;
+}NotificationProviderData;
+
+NotificationError
+SubscribeNotification(NotificationHandler handler,
+                      NotificationType type,
+                      uint64_t specificEvent)
+{
+
+}
+
+NotificationError
+UnsubscribeNotfication(NotificationHandler handler,
+                       NotificationType type,
+                       uint64_t specificEvent)
+{
+
+}
+
+UID
+RegisterNotifyingSystem(const char *sys_name,
+						NotificationType t,
+						uint64_t n,
+						NotificationSubscriptionHandler handler)
+{
+
+}
+
+NotificationError
+PostNotification(UID subID,
+				 NotificationType t,
+				 uint64_t n)
+{
+
+}
--- managers/notification_manager/notifications.h
+++ managers/notification_manager/notifications.h
@@ -8,16 +8,35 @@ typedef enum NotificationType {
     NotificationType_Performance
 } NotificationType;
 
-typedef (void)(*NotificationHandler)(NotificationType t, uint64_t n);
+typedef enum NotificationError{
+	NotificationError_Success,
+	NotificationError_Failed,
+	NotificationError_InvalidUID
+}
 
-void
+typedef (void)(*NotificationHandler)(NotificationType t, uint64_t n);
+typedef (NotificationError)(*NotificationSubscriptionHandler)(NotificationHandler h, NotificationType t, uint64_t n);
+
+
+NotificationError
 SubscribeNotification(NotificationHandler handler,
                       NotificationType type,
                       uint64_t specificEvent);
 
-void
+NotificationError
 UnsubscribeNotfication(NotificationHandler handler,
                        NotificationType type,
                        uint64_t specificEvent);
+
+UID
+RegisterNotifyingSystem(const char *sys_name,
+						NotificationType t,
+						uint64_t n,
+						NotificationSubscriptionHandler handler);
+
+NotificationError
+PostNotification(UID subID,
+				 NotificationType t,
+				 uint64_t n);
 
 #endif
