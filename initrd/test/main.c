8c05adab9bb8370769948ccd78b3816c5e84b077
Himanshu Goel <himanshu.goel2797@gmail.com>
2016-06-30 22:09:39 -0600

    Astyle

--- initrd/test/main.c
+++ initrd/test/main.c
@@ -1,16 +1,15 @@
 #include "libs/libCardinal/include/syscall.h"
 
-int _start()
-{
-	uint64_t par = 0;
-	Syscall(0, 1, &par, 0, NULL);
+int _start() {
+    uint64_t par = 0;
+    Syscall(0, 1, &par, 0, NULL);
 
-		__asm__ volatile(
-			"loop:\n\t"
-			"pushfq\n\t"
-			"popq %%rax\n\t"
-			"jmp loop\n\t" :::
-			);
+    __asm__ volatile(
+        "loop:\n\t"
+        "pushfq\n\t"
+        "popq %%rax\n\t"
+        "jmp loop\n\t" :::
+    );
 
-	return 0;
+    return 0;
 }
