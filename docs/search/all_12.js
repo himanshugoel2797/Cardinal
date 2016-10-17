2e36e49884b9245eb94f91df57ba0aab90a3aa72
Himanshu Goel <himanshu.goel2797@gmail.com>
2016-10-16 21:43:39 -0400

    Rebuilt documentation

--- docs/search/all_12.js
+++ docs/search/all_12.js
@@ -5,6 +5,7 @@ var searchData=
   ['taskswitch',['TaskSwitch',['../d6/d5a/group__threading.html#ga261a137d70c2ca33f48f4e88181c8138',1,'TaskSwitch(uint32_t int_no, uint32_t err_code):&#160;thread.c'],['../d6/d5a/group__threading.html#ga261a137d70c2ca33f48f4e88181c8138',1,'TaskSwitch(uint32_t int_no, uint32_t err_code):&#160;thread.c']]],
   ['terminateprocess',['TerminateProcess',['../d3/d12/group__proc__man.html#gab64609ca5f040abc1f53a6f053a7cf74',1,'TerminateProcess(UID pid, uint32_t exit_code):&#160;process.c'],['../d3/d12/group__proc__man.html#gab64609ca5f040abc1f53a6f053a7cf74',1,'TerminateProcess(UID pid, uint32_t exit_code):&#160;process.c']]],
   ['thread_5finitialize',['Thread_Initialize',['../d6/d5a/group__threading.html#ga2876b648540d1904a14cbe08d679da5e',1,'Thread_Initialize(void):&#160;thread.c'],['../d6/d5a/group__threading.html#ga2876b648540d1904a14cbe08d679da5e',1,'Thread_Initialize(void):&#160;thread.c']]],
+  ['threading_20syscalls',['Threading Syscalls',['../d8/dba/group__thread__syscall.html',1,'']]],
   ['threadentrypoint',['ThreadEntryPoint',['../d6/d5a/group__threading.html#gadc80c3fd3adb03b4253e50f08974eda6',1,'thread.h']]],
   ['threadids',['ThreadIDs',['../d4/d03/structProcessInformation.html#a48615b491b5f02a51843631be1d658ff',1,'ProcessInformation']]],
   ['threadinfo',['ThreadInfo',['../d5/dfe/structThreadInfo.html',1,'ThreadInfo'],['../d6/d5a/group__threading.html#ga677d11d684c06c2113e0515aeab2adfd',1,'ThreadInfo():&#160;thread.h']]],
--- docs/search/groups_7.js
+++ docs/search/groups_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['threading_20syscalls',['Threading Syscalls',['../d8/dba/group__thread__syscall.html',1,'']]],
   ['threading',['Threading',['../d6/d5a/group__threading.html',1,'']]],
   ['timer',['Timer',['../dc/d8f/group__timer__hal.html',1,'']]]
 ];
