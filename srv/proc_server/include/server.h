e8be3cd2deb39a00ad1f141f70aad4378a09921e
Himanshu Goel <himanshu.goel2797@gmail.com>
2016-11-27 01:00:28 -0500

    Detailed key system design

--- srv/proc_server/include/server.h
+++ srv/proc_server/include/server.h
@@ -17,4 +17,12 @@ typedef struct {
 	}
 } ProcServ_CreateRequest_Response;
 
+
+//All keys are stored in kernel, in a 'sparse' array, a total of 1GiB in size in virtual memory.
+//Each process can create up to 16384 keys (hard hard limit), hard limit of 4096, soft limit of 1024, each being a struct of 32 bytes.
+//This allows for a maximum of 2048 simultaneous processes across all users on the computer.
+//The lower 32 bits of every key represent its index into the table, with the higher parts being random.
+//The key is the first 8 bytes of every entry, with the second 8 bytes being an identifier provided by the application allocating the descriptor.
+//The next 8 bytes is the creating process's UID, and the last 8 bytes represent various flags relating to the persistence of the key.
+//
 #endif
