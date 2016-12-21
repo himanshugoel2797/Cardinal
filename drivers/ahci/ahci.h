114ea20da87eafaa544d66398f801c4b53ac986b
Himanshu Goel <himanshu.goel2797@gmail.com>
2016-12-21 00:12:12 -0500

    AHCI driver now capable of reads

---
+++ drivers/ahci/ahci.h
@@ -0,0 +1,106 @@
+#ifndef _CARDINAL_AHCI_DRIVER_H_
+#define _CARDINAL_AHCI_DRIVER_H_
+
+#include <cardinal/cardinal_types.h>
+
+typedef enum {
+	ATA_CMD_READ_DMA_EXT = 0x25
+} ATA_CMD;
+
+typedef enum {
+	FISType_RegisterH2D = 0x27,
+	FISType_RegisterD2H = 0x34,
+	FISType_DMAActivate = 0x39,
+	FISType_DMASetup = 0x41,
+	FISType_Data = 0x46,
+} FISType;
+
+typedef struct {
+	uint8_t fisType;
+	struct{
+		uint8_t pm_port : 4;
+		uint8_t rsv0 : 3;
+		uint8_t cmd : 1;
+	};
+	uint8_t command;
+	uint8_t features_lo;
+	uint16_t lba_lo;
+	uint8_t lba_mid;
+	uint8_t device;
+	uint16_t lba_mid_h;
+	uint8_t lba_hi;
+	uint8_t features_hi;
+	uint16_t count;
+	uint8_t icc;
+	uint8_t control;
+	uint32_t rsv1;
+} RegisterH2D_FIS;
+
+typedef struct {
+
+} RegisterD2H_FIS;
+
+typedef struct {
+
+} DMASetupFIS;
+
+typedef struct {
+
+} PIOSetupFIS;
+
+typedef struct {
+
+} Device2HostFIS;
+
+typedef struct {
+
+} SDBsFIS;
+
+typedef struct {
+	DMASetupFIS dma;
+	uint8_t rsv0[8];
+	PIOSetupFIS ps;
+	uint8_t rsv1[12];
+	Device2HostFIS d2h;
+	uint8_t rsv2[4];
+	SDBsFIS sdbs;
+} FISLayout;
+
+typedef struct {
+	uint16_t cfl : 5;
+	uint16_t atapi : 1;
+	uint16_t write : 1;
+	uint16_t prefetch : 1;
+	uint16_t reset : 1;
+	uint16_t bist : 1;
+	uint16_t dc : 6;
+} AHCICommand_Info;
+
+typedef struct {
+	AHCICommand_Info info;
+	uint16_t PRDTL;
+	uint32_t byteCount;
+	uint32_t commandTableBaseAddress;
+	uint32_t commandTableBaseAddressUpper;
+	uint32_t rsv[4];
+} AHCICommand;
+
+typedef struct {
+	uint32_t baseAddress;
+	uint32_t baseAddressUpper;
+	uint32_t rsv0;
+	union {
+		uint32_t byteCount : 22;
+		uint32_t rsv1 : 9;
+		uint32_t intCompletion : 1;
+	};
+} AHCI_PRDT;
+
+typedef struct {
+	uint8_t cmd_fis[64];
+	uint8_t atapiCMD[16];
+	uint8_t rsv[48];
+	AHCI_PRDT prdt[128];
+} AHCICommandTable;
+
+#endif
--- drivers/ahci/main.c
+++ drivers/ahci/main.c
@@ -6,9 +6,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "ahci.h"
+
 //Register offsets
 #define HBA_GHC 0x4
 #define HBA_PI 0xC
+#define HBA_BOHC 0x28
 #define HBA_PxCMD(x) (0x118 + 0x80 * x)
 #define HBA_PxSERR(x) (0x130 + 0x80 * x)
 #define HBA_PxTFD(x) (0x120 + 0x80 * x)
@@ -17,7 +20,9 @@
 #define HBA_PxCLBU(x) (0x104 + 0x80 * x)
 #define HBA_PxFB(x) (0x108 + 0x80 * x)
 #define HBA_PxFBU(x) (0x10C + 0x80 * x)
-
+#define HBA_PxIS(x) (0x110 + 0x80 * x)
+#define HBA_PxSACT(x) (0x134 + 0x80 * x)
+#define HBA_PxCI(x) (0x138 + 0x80 * x)
 
 //Register bits and masks
 #define HBA_PxCMD_ST (1 << 0)
@@ -37,6 +42,9 @@
 static uint64_t PCI_BAR;
 
 static uint32_t activeDevices;
+static uint32_t deviceCount;
+
+static uint64_t dma_vaddr;
 
 void
 Write8(uint32_t off, uint8_t val) {
@@ -79,6 +87,11 @@ ResetHBA(void) {
 }
 
 void
+ObtainOwnership(void) {
+    Write32(HBA_BOHC, (1 << 1));    //Obtain ownership of the HBA
+}
+
+void
 ReportAHCIAwareness(void) {
     Write32(HBA_GHC, (1 << 31));
 }
@@ -108,6 +121,9 @@ InitializePort(int index, uint32_t dma_base) {
 
     }
 
+    if(Read32(HBA_PxSACT(index)) != 0)
+        __asm__("hlt");
+
     //Command buffer is 1024 bytes
     Write32(HBA_PxCLB(index), dma_base + (index * CMD_BUF_SIZE));
     Write32(HBA_PxCLBU(index), 0);
@@ -115,6 +131,15 @@ InitializePort(int index, uint32_t dma_base) {
     //FIS buffer is 256 bytes
     Write32(HBA_PxFB(index), dma_base + 32 * CMD_BUF_SIZE + (index * FIS_SIZE));
     Write32(HBA_PxFBU(index), 0);
+
+    //Setup the first command table entry
+    AHCICommand *cmd_entry = (AHCICommand*)(dma_vaddr + (index * CMD_BUF_SIZE));
+    for(int i = 0; i < 32; i++){
+        cmd_entry->PRDTL = 64;
+        cmd_entry->commandTableBaseAddress = (dma_base + 32 * (CMD_BUF_SIZE + FIS_SIZE) + index * sizeof(AHCICommandTable));
+        cmd_entry->commandTableBaseAddressUpper = 0;
+        cmd_entry++;
+    }
 
     //Enable receiving FIS's
     Write32(HBA_PxCMD(index), Read32(HBA_PxCMD(index)) | HBA_PxCMD_FRE);
@@ -139,8 +164,84 @@ InitializePort(int index, uint32_t dma_base) {
 
     //The device is functional, mark it as so and start it up
     activeDevices |= (1 << index);
+    deviceCount++;
 
     Write32(HBA_PxCMD(index), Read32(HBA_PxCMD(index)) | HBA_PxCMD_ST);
+}
+
+int GetCommandSlot(int index) {
+    uint32_t cmd_slots = Read32(HBA_PxSACT(index));
+
+    for(int i = 0; i < 32; i++)
+        if(~cmd_slots & (1 << i))
+            return i;
+
+    return -1;
+}
+
+int ReadDevice(int index, uint64_t loc, void *addr, uint32_t len) {
+
+    uint32_t target_len = len;
+    if(target_len % (32 * 1024))
+        target_len -= target_len % (32 * 1024);
+
+    //Clear interrupt flags
+    Write32(HBA_PxIS(index), 0xFFFFFFFF);
+
+    int slot = GetCommandSlot(index);
+
+    AHCICommand *ahci_cmd = (AHCICommand*)(dma_vaddr + (index * CMD_BUF_SIZE));
+    ahci_cmd += slot;
+
+    ahci_cmd->info.cfl = sizeof(RegisterH2D_FIS)/sizeof(uint32_t);
+    ahci_cmd->info.write = 0;
+    ahci_cmd->info.atapi = 0;
+
+    ahci_cmd->PRDTL = (target_len / (32 * 1024));
+
+    AHCICommandTable *cmd_table = (AHCICommandTable*)(dma_vaddr + 32 * (CMD_BUF_SIZE + FIS_SIZE) + index * sizeof(AHCICommandTable));
+
+    uint64_t p_addr = 0;
+    R01_GetPhysicalAddress(0, (uint64_t)addr, &p_addr);
+    uint64_t p_curaddr = p_addr;
+
+    int i = 0;
+    for(; i < 127 && target_len > 32 * 1024; i++){
+        cmd_table->prdt[i].baseAddress = (uint32_t)p_curaddr;
+        cmd_table->prdt[i].baseAddressUpper = (uint32_t)(p_curaddr >> 32);
+        cmd_table->prdt[i].rsv0 = 0;
+        cmd_table->prdt[i].rsv1 = 0;
+        cmd_table->prdt[i].byteCount = 32 * 1024;
+        cmd_table->prdt[i].intCompletion = 0;
+
+        target_len -= 32 * 1024;
+        p_curaddr += 32 * 1024;
+    }
+    cmd_table->prdt[i].baseAddress = (uint32_t)p_curaddr;
+    cmd_table->prdt[i].baseAddressUpper = (uint32_t)(p_curaddr >> 32);
+    cmd_table->prdt[i].rsv0 = 0;
+    cmd_table->prdt[i].rsv1 = 0;
+    cmd_table->prdt[i].byteCount = target_len;
+    cmd_table->prdt[i].intCompletion = 0;
+
+
+    RegisterH2D_FIS *fis = (RegisterH2D_FIS*)cmd_table->cmd_fis;
+    fis->fisType = FISType_RegisterH2D;
+    fis->cmd = 1;
+    fis->command = ATA_CMD_READ_DMA_EXT;
+    fis->lba_lo = (uint16_t)loc;
+    fis->lba_mid = (uint8_t)(loc >> 16);
+    fis->lba_mid_h = (uint16_t)(loc >> 24);
+    fis->lba_hi = (uint8_t)(loc >> 40);
+
+    fis->device = 1 << 6;   //LBA48 mode
+    fis->count = target_len/512;
+
+    Write32(HBA_PxSACT(index), 1 << slot);
+    Write32(HBA_PxCI(index), 1 << slot);
+
+    while(Read32(HBA_PxCI(index)) & (1 << slot));
+
 }
 
 int main(int argc, char *argv[]) {
@@ -164,6 +265,11 @@ int main(int argc, char *argv[]) {
 
     PCI_EnableBusMaster(&device);
 
+    //Obtain ownership of the HBA from the BIOS
+    ObtainOwnership();
+    //Wait 2 seconds to allow the BIOS to finish up any commands
+    //SleepThread(2 * 1000 * 1000 * 1000);
+    
     //Report that the OS is now AHCI aware
     ReportAHCIAwareness();
     ResetHBA();
@@ -178,22 +284,37 @@ int main(int argc, char *argv[]) {
     uint64_t dma_buffer = 0;
     uint64_t dma_phys_addr = 0;
     MMap(&dma_buffer,
-         32 * (CMD_BUF_SIZE + FIS_SIZE),
+         32 * (CMD_BUF_SIZE + FIS_SIZE + sizeof(AHCICommandTable)),
          MemoryAllocationFlags_Write | MemoryAllocationFlags_Read,
          MMapFlags_DMA,
          CachingModeWriteBack
         );
     R01_GetPhysicalAddress(0, dma_buffer, &dma_phys_addr);
 
-    memset((void*)dma_buffer, 0, 32 * (CMD_BUF_SIZE + FIS_SIZE));
+    memset((void*)dma_buffer, 0, 32 * (CMD_BUF_SIZE + FIS_SIZE + sizeof(AHCICommandTable)));
+
 
     //Initialize the active device bitmap
     activeDevices = 0;
+    deviceCount = 0;
+    dma_vaddr = dma_buffer;
 
     //Initialize the implemented ports
     for(int i = 0; i < 32; i++)
         if(ports_implemented & (1 << i))
             InitializePort(i, dma_phys_addr);
+
+    uint64_t rd_dma_buffer = 0;
+    uint64_t rd_dma_phys_addr = 0;
+    MMap(&rd_dma_buffer,
+         32 * 1024,
+         MemoryAllocationFlags_Write | MemoryAllocationFlags_Read,
+         MMapFlags_DMA,
+         CachingModeWriteBack
+        );
+    memset(rd_dma_buffer, 0xFF, 32 * 1024);
+
+    ReadDevice(0, 0, rd_dma_buffer, 32 * 1024);
 
     while(1) {
 
