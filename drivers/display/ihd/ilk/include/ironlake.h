0b560867dd7442d935ca204d64dcb43b9e6aed9e
Himanshu Goel <himanshu.goel2797@gmail.com>
2017-01-12 17:16:11 -0500

    Refactored Intel drivers to be able to share source code where possible

---
+++ drivers/display/ihd/ilk/include/ironlake.h
@@ -0,0 +1,16 @@
+#ifndef _IHD_IRONLAKE_H_
+#define _IHD_IRONLAKE_H_
+
+#define HDMI_C_INDEX 0
+#define HDMI_D_INDEX 1
+#define LVDS_INDEX 2
+
+#define PF_A_INDEX 0
+#define PF_B_INDEX 1
+
+#define DISPLAY_COUNT 3
+#define PF_COUNT 2
+#define PIPE_COUNT 2
+#define FDI_COUNT 2
+
+#endif
