2d6431aaf9b23a8e9402ff3a731fe617fc07cba4
Himanshu Goel <himanshu.goel2797@gmail.com>
2017-01-30 20:53:50 -0500

    Fixed BTree index bug

--- kernel/common/btree.c
+++ kernel/common/btree.c
@@ -1,14 +1,10 @@
-/*
-The MIT License (MIT)
+/**
+ * Copyright (c) 2017 Himanshu Goel
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
-Copyright (c) 2016-2017 Himanshu Goel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
 #include "btree.h"
 #include "kmalloc.h"
 #include "memory.h"
@@ -239,18 +235,19 @@ BTree_Delete(BTree *h) {
 uint64_t
 BTree_GetKey(BTree *h) {
     LockSpinlock(h->key_lock);
-    uint64_t cur_key = h->key++;
-    //TODO this routine is bugged, fix it
-    for(int i = 0; i < h->max_levels; i++) {
+    
+    uint64_t key = h->key++;
+    for(int i = 0; i < h->max_levels; i++){
+        int val = (key >> (i * BITS_PER_LEVEL)) & LEVEL_INDEX_MASK;
 
-        int pos = (cur_key >> (i * BITS_PER_LEVEL)) & LEVEL_INDEX_MASK;
-
-        if(pos > INDEX_MAX) {
-            cur_key += ((1 << BITS_PER_LEVEL) - pos) << (i * BITS_PER_LEVEL);
+        if(val > INDEX_MAX){
+            key &= ~(uint64_t)(LEVEL_INDEX_MASK << (i * BITS_PER_LEVEL));
+            key += (1 << ((i + 1) * BITS_PER_LEVEL));
         }
     }
+    h->key = key + 1;
     UnlockSpinlock(h->key_lock);
-    return cur_key;
+    return key;
 }
 
 uint64_t
